431aadb273634aa688f35ce2259623bbf0a31880
Knowles,Andrew <andrewdale166@gmail.com>
2019-01-25 16:57:09 -0600

    ICP1

---
+++ ICP1.py
@@ -0,0 +1,20 @@
+x = input("Enter a 3 digit number")
+print(x[::])
+
+x = input("enter number")
+y = input("enter number")
+
+print("%s times %s equals %s" %(x, y, int(x)*int(y)))
+
+x = input("enter a sentence")
+letters = 0
+words = 1
+numbers = 0
+for i in range(len(x)):
+    if x[i].isalpha():
+        letters+=1
+    if x[i].isspace():
+        words+=1
+    if x[i].isnumeric():
+        numbers+=1
+print("Letters: %i Words: %i Numbers: %i" %(letters, words, numbers))
